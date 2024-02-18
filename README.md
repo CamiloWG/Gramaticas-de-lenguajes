f10b4fa305c0001291ba90c88a5ff26c70378af8
Camilo Madero <smadero@wi-mobile.com>
2024-02-17 20:50:34 -0500

    Gramatica 5 agregada

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Gramaticas-de-lenguajes
 Programas de Python para interpretar diferentes gramaticas
+
+<br>
+
+---------
+# Diagrama de estados de automatas
+
+
+![Modelo Automata G5](./src/model-g5.png)
+Imágen 5. Diagrama de estados finitos para el autómata de la grámatica 5.
+
---
+++ gramatica_cinco.py
@@ -0,0 +1,48 @@
+
+
+class Automata:
+    
+    def __init__(self, texto):
+        self.texto = texto
+    
+    def validar_cadena(self):
+        estado = 1
+
+        for i in self.texto:
+            if i in ['a', 'b']:
+                if estado == 1:
+                    if i == 'a':
+                        estado = 2
+                    else: 
+                        return False
+                elif estado == 2:
+                    if i == 'a':
+                        estado = 3
+                    elif i == 'b':
+                        estado = 4
+                    else:
+                        return False
+                elif estado == 3:
+                    if i == 'b':
+                        estado = 2
+                    else:
+                        return False
+                elif estado == 4:
+                    return False
+        return estado == 4
+    
+    def print_result(self):
+        return print('La cadena', self.texto, 'es CORRECTA!' if self.validar_cadena() else 'es INCORRECTA!')
+    
+
+
+def main():
+    palabra = input('Ingrese la palabra:\n')
+    reader = Automata(palabra)
+    reader.print_result();
+
+
+if __name__ == '__main__':
+    main()
+    
+                        
